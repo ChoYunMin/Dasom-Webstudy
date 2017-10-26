63d35f3da3d036fde6349b89278e504747a3cc7a
ChoYunMin <dbsals0618@naver.com>
2017-10-27 02:29:24 +0900

    first init

---
+++ DASOM/main.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="utf-8">
+     <link rel="stylesheet" type="text/css" href="http://localhost/DASOM/mainstyle.css">
+     <title>DASOM</title>
+</head>
+<body>
+  <div class="tab">
+    <section class="tab_image" style="background-image: url('tab01_image.jpeg');"></section>
+    <section class="black_mask"></section>
+    <div class="DASOM">
+      <div style="margin-bottom: -5vmax;">
+        <span id="D" style="-webkit-text-stroke-color: #FFFFFF;-webkit-text-stroke-width: 0.15vmax;">
+          D</span>
+        <span id="ASOM">ASOM</span>
+      </div>
+      <span id="khu">KYUNG HEE UNIVERSITY COMPUTER ENGINEERING STUDY CLUB</span>
+    </div>
+  </div>
+
+  <div class="tab" style="background-color: #FFFFFF;">
+  </div>
+
+  <div class="tab" style="background-color: #E1E4EC;">
+  </div>
+
+  <div class="tab" style="background-color: #FFFFFF;">
+  </div>
+
+
+</body>
+</html>
